f21db3daf2b08d555ca3f8098e63535adc1b6f3b
JotaClase <gongorarealjota@gmail.com>
2021-06-07 13:59:46 +0200

    JSON en PHP

--- public/index.php
+++ public/index.php
@@ -1,30 +1,10 @@
-<!-- <!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-<h1>Hola mundo</h1>
-<p>Hoy es <strong><?= date("l")?></strong>. ¿Qué tal estás?</p>
-</body>
-</html> -->
-
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <title>PHP Exercice</title>
-    <meta name="viewport" content="width=device-width,initial-scale=1">
-    <link rel="stylesheet" href="./css/main.css" type="text/css">
-    <script src="./js/main.js"></script>
-</head>
+<?php require('./head.php');?>
 
 <body>
-<?php include('./links.php')?>
+<?php include('./links.php');?>
   
     <?php
     $weeekDays = [
@@ -39,6 +19,8 @@
     ?>
     <h1>Hola, <?=$_GET['name']?></h1>
     <p>Hoy es <strong><?= $weeekDays[date('N')]; ?></strong>. ¿Qué tal estás?</p>
+
+    <?php require('./footer.php'); ?>
 </body>
 
 </html>
