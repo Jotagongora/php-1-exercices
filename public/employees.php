9c9b10fd63010bf8db54070db9448a91ee2512da
JotaClase <gongorarealjota@gmail.com>
2021-06-09 13:54:57 +0200

    get y post php

--- public/employees.php
+++ public/employees.php
@@ -1,8 +1,21 @@
 <?php require $_SERVER['DOCUMENT_ROOT'] . '/lib/app.php';
 
-$query = isset($_GET['id']) ? "SELECT * FROM employees WHERE id={$_GET['id']}" : "SELECT * FROM employees";
+$query = 'SELECT * FROM employees';
+if (isset($_GET['id'])) {
+$query = 'SELECT * FROM employees WHERE id = :identificador';
+} elseif (isset($_GET['email'])) {
+$query = 'SELECT * FROM employees WHERE email = :correo';
+}
 
-$stm = $mdbConnexion->query($query);
+$stm = $mdbConnexion->prepare($query); // prepare en vez de query te protege de ataques de inyección SQL
+
+if (isset($_GET['id'])) {
+    $stm->bindParam(':identificador', $_GET['id']);
+    } elseif (isset($_GET['email'])) {
+    $stm->bindParam(':correo', $_GET['email']);
+    }
+
+$stm->execute();
 
 $people = $stm->fetchAll(PDO::FETCH_ASSOC);
 
